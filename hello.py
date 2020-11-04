d1830b2b2d10a5c58966a0b8c3f656e07723f8db
Antoine BOUARD <antoinebouardain@gmail.com>
2020-11-04 12:13:42 +0100

    Test getAllUsers

--- hello.py
+++ hello.py
@@ -1,6 +1,66 @@
-from flask import Flask
+import sqlite3
+
+import click
+from flask import Flask, current_app, g, render_template
+from flask.cli import with_appcontext
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/testgetallusers')
+def testGetAllUsers():
+    users = get_all_users()
+    return render_template('testusers.html', users=users)
+
+@app.route('/testuser/<username>')
+def user(username=None):
+    return render_template('testuserdetails.html', username=username)
+
+
+# BDD
+
+def get_all_users():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM user")
+    return cur.fetchall()
+    
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            'instance/bdd.db',
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with app.open_resource('script.sql') as f:
+        # Pour éxécuter du script SQL
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+app.teardown_appcontext(close_db)
+app.cli.add_command(init_db_command)
